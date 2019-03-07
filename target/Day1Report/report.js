8f38d1b26743bbb1369f1b3e8d959ad9cbf0882e
MuhtarMahmut <Muhtar.Mahmut0519@yahoo.com>
2019-03-07 15:13:05 -0500

    s

---
+++ target/Day1Report/report.js
@@ -0,0 +1,375 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Features/DDT.feature");
+formatter.feature({
+  "name": "Testing Multi-Datas",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "search \u0027\u003cNewSearch\u003e\u0027 keyword",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "NewSearch"
+      ]
+    },
+    {
+      "cells": [
+        "Lap-top"
+      ]
+    },
+    {
+      "cells": [
+        "Potato"
+      ]
+    },
+    {
+      "cells": [
+        "Computer"
+      ]
+    },
+    {
+      "cells": [
+        "Ipad"
+      ]
+    },
+    {
+      "cells": [
+        "Iphone"
+      ]
+    },
+    {
+      "cells": [
+        "samsung"
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SearchStep.environemnt_is_ready()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SearchStep.got_to_the_Website(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search \u0027Lap-top\u0027 keyword",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SearchStep.search_keyword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchStep.close_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SearchStep.environemnt_is_ready()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SearchStep.got_to_the_Website(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search \u0027Potato\u0027 keyword",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SearchStep.search_keyword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchStep.close_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SearchStep.environemnt_is_ready()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SearchStep.got_to_the_Website(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search \u0027Computer\u0027 keyword",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SearchStep.search_keyword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchStep.close_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SearchStep.environemnt_is_ready()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SearchStep.got_to_the_Website(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search \u0027Ipad\u0027 keyword",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SearchStep.search_keyword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchStep.close_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SearchStep.environemnt_is_ready()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SearchStep.got_to_the_Website(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search \u0027Iphone\u0027 keyword",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SearchStep.search_keyword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchStep.close_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Google Searches3",
+  "description": "",
+  "keyword": "Scenario Template",
+  "tags": [
+    {
+      "name": "@DDT2"
+    }
+  ]
+});
+formatter.step({
+  "name": "Environemnt is ready",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SearchStep.environemnt_is_ready()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "got to the Website \u0027https://www.google.com\u0027",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "SearchStep.got_to_the_Website(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "search \u0027samsung\u0027 keyword",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "SearchStep.search_keyword(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "SearchStep.close_the_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
